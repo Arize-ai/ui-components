dc40b7a144f402e293148bfb37930bcb00ae0799
Mikyo King <mikeldking@gmail.com>
2021-01-28 16:43:33 -0700

    Modify alert

--- src/Alert.tsx
+++ src/Alert.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react';
 import { css } from '@emotion/core';
 
 export type AlertProps = {
-  type: 'warning';
+  type: 'warning' | 'info';
   message: ReactNode;
 };
 
