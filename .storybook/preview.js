271ecd92c6c3bc91b2e288788ec18b8c3b02b6b5
Mikyo King <mikeldking@gmail.com>
2021-03-30 13:18:01 -0600

    add arize background color

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,4 +2,14 @@
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
   actions: { argTypesRegex: '^on.*' },
+  // https://storybook.js.org/docs/react/essentials/backgrounds
+  backgrounds: {
+    default: 'arize',
+    values: [
+      {
+        name: 'arize',
+        value: '#14171b',
+      },
+    ],
+  },
 };
