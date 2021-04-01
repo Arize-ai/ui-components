89c45a69b71b40db6a41091c891245d632ff6e4e
Mikyo King <mikeldking@gmail.com>
2021-04-01 14:52:38 -0600

    Links support and onclick

--- stories/List.stories.tsx
+++ stories/List.stories.tsx
@@ -43,3 +43,30 @@ const Template: Story<ListProps> = args => (
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
 export const Default = Template.bind({});
+
+const WithLinksTemplate: Story<ListProps> = args => (
+  <Card title="Model Links" bodyStyle={{ padding: 0 }} style={{ width: 300 }}>
+    <List {...args} noPadding={true}>
+      {Array.from('12345').map(num => (
+        <ListItem key={num}>
+          <a
+            css={css`
+              display: flex;
+              flex-direction: column;
+              padding: 16px;
+              text-decoration: none;
+            `}
+            href="#"
+          >
+            <Text size="medium" weight="heavy">{`List Item ${num}`}</Text>
+            <Text size="small" color="white70">{`Subtext`}</Text>
+          </a>
+        </ListItem>
+      ))}
+    </List>
+  </Card>
+);
+
+// By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
+// https://storybook.js.org/docs/react/workflows/unit-testing
+export const WithLinks = WithLinksTemplate.bind({});
