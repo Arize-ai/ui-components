b65cf30ee3abff9535c0e02d79ad31af7bfda59e
Eunice Kokor <eunicekokor@gmail.com>
2021-04-13 15:03:45 -0400

    format

--- src/radio/styles.ts
+++ src/radio/styles.ts
@@ -1,28 +1,32 @@
 import theme from '../theme';
 import { css } from '@emotion/core';
 
-export const radioGroupCSS = ({ isDisabled = false }: { isDisabled: boolean }) => css`
+export const radioGroupCSS = ({
+  isDisabled = false,
+}: {
+  isDisabled: boolean;
+}) => css`
   color: ${isDisabled ? theme.colors.disabled : theme.colors.text1};
 `;
 
 export const radioCSS = ({
-	isFocusVisible = false,
-	isDisabled = false,
-  }: {
-	isFocusVisible?: boolean;
-	isDisabled?: boolean;
-  }) => css`
-	display: flex;
-	color: ${isDisabled ? theme.colors.disabled : theme.colors.text1};
-	font-size: ${theme.typography.sizes.small.fontSize}px;
-	line-height: ${theme.typography.sizes.small.lineHeight}px;
-	svg {
-	  fill: ${isDisabled ? theme.colors.disabled : theme.colors.text1};
-	  margin-right: ${theme.spacing.padding8}px;
-	  height: 16px;
-	  width: 16px;
-	  ${isFocusVisible && `outline: 2px solid -webkit-focus-ring-color;`}
-	}
-	align-items: center;
-	cursor: ${isDisabled ? 'not-allowed' : 'pointer'};
-  `;
+  isFocusVisible = false,
+  isDisabled = false,
+}: {
+  isFocusVisible?: boolean;
+  isDisabled?: boolean;
+}) => css`
+  display: flex;
+  color: ${isDisabled ? theme.colors.disabled : theme.colors.text1};
+  font-size: ${theme.typography.sizes.small.fontSize}px;
+  line-height: ${theme.typography.sizes.small.lineHeight}px;
+  svg {
+    fill: ${isDisabled ? theme.colors.disabled : theme.colors.text1};
+    margin-right: ${theme.spacing.padding8}px;
+    height: 16px;
+    width: 16px;
+    ${isFocusVisible && `outline: 2px solid -webkit-focus-ring-color;`}
+  }
+  align-items: center;
+  cursor: ${isDisabled ? 'not-allowed' : 'pointer'};
+`;
