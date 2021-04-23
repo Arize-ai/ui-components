e81424e8c6b0d1ecbd265ce21b7bd2cc02feb15b
Mikel King <mikeldking@gmail.com>
2021-04-23 15:44:17 -0600

    Test in example

--- example/index.tsx
+++ example/index.tsx
@@ -3,19 +3,55 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { jsx } from '@emotion/core';
-import { Provider, ActionButton, PopoverTrigger, List, ListItem Card } from '../.';
+import {
+  Provider,
+  ActionButton,
+  PopoverTrigger,
+  List,
+  ListItem,
+  Card,
+  TabbedCard,
+  Tabs,
+} from '../.';
 
+const { TabPane } = Tabs;
 const App = () => {
   return (
     <Provider>
-      <div>
-        <PopoverTrigger>
-          <ActionButton>Click Me</ActionButton>
-          <div style={{ border: '1px solid gray' }}>Popover content here</div>
-        </PopoverTrigger>
-        <Card title={"Hello World"}>
-        <List><ListItem>Item One</ListItem></List>
+      <div style={{ display: 'flex' }}>
+        <Card
+          title={'Hello World'}
+          extra={
+            <PopoverTrigger>
+              <ActionButton>Click Me</ActionButton>
+              <div style={{ border: '1px solid gray' }}>
+                Popover content here
+              </div>
+            </PopoverTrigger>
+          }
+        >
+          <List>
+            <ListItem>Item One</ListItem>
+          </List>
         </Card>
+        <TabbedCard>
+          <Tabs>
+            <TabPane name="Tab One">
+              <List>
+                <ListItem>Item One</ListItem>
+                <ListItem>Item Two</ListItem>
+                <ListItem>Item Three</ListItem>
+              </List>
+            </TabPane>
+            <TabPane name="Tab Two">
+              <List>
+                <ListItem>Tab Two Item One</ListItem>
+                <ListItem>Tab Two Item Two</ListItem>
+                <ListItem> Tab Two Item Three</ListItem>
+              </List>
+            </TabPane>
+          </Tabs>
+        </TabbedCard>
       </div>
     </Provider>
   );
