888e88fc81cb82883409aac420c103ce145931f8
Mikel King <mikeldking@gmail.com>
2021-08-20 18:47:11 -0600

    fix test

--- test/Button.test.tsx
+++ test/Button.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import Button from '../src/Button';
+import { Button } from '../src';
 
 describe('Button', () => {
   it('renders without crashing', () => {
