1cf218624b7fa4dc6792bb8b3ed3434bf2062698
Mikel King <mikeldking@gmail.com>
2021-11-23 17:20:12 -0700

    Add controlled example

--- stories/Picker.stories.tsx
+++ stories/Picker.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { Item, Picker, PickerProps } from '../src';
+import { Item, Picker, PickerProps, Text } from '../src';
 import { Provider } from '../src';
 
 const meta: Meta = {
@@ -35,15 +35,48 @@ const Template: Story<PickerProps<string>> = args => (
 // https://storybook.js.org/docs/react/workflows/unit-testing
 export const Default = Template.bind({});
 
-const WithAddon: Story<PickerProps<string>> = args => (
-  <Provider>
-    <Picker addonBefore={'Frequency'} {...args}>
-      <Item key="rarely">Rarely</Item>
-      <Item key="sometimes">Sometimes</Item>
-      <Item key="always">Always</Item>
-    </Picker>
-  </Provider>
-);
+const Controlled: Story<PickerProps<string>> = args => {
+  const [frequency, setFrequency] = React.useState<string>('rarely');
+  return (
+    <Provider>
+      <Picker
+        {...args}
+        selectedKey={frequency}
+        onSelectionChange={selected => setFrequency(selected as string)}
+      >
+        <Item key="rarely">Rarely</Item>
+        <Item key="sometimes">Sometimes</Item>
+        <Item key="always">Always</Item>
+      </Picker>
+      <br />
+      <Text>Selected Value: {frequency}</Text>
+    </Provider>
+  );
+};
+
+// By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
+// https://storybook.js.org/docs/react/workflows/unit-testing
+export const controlled = Controlled.bind({});
+
+const WithAddon: Story<PickerProps<string>> = args => {
+  const [frequency, setFrequency] = React.useState<string>('rarely');
+  return (
+    <Provider>
+      <Picker
+        addonBefore={'Frequency'}
+        {...args}
+        selectedKey={frequency}
+        onSelectionChange={selected => setFrequency(selected as string)}
+      >
+        <Item key="rarely">Rarely</Item>
+        <Item key="sometimes">Sometimes</Item>
+        <Item key="always">Always</Item>
+      </Picker>
+      <br />
+      <Text>Selected Value: {frequency}</Text>
+    </Provider>
+  );
+};
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
