e17308abcc4aeacf27defa82dcb1fe2b3de46fc4
Mikyo King <mikeldking@gmail.com>
2022-06-08 16:40:33 -0700

    Export more types, add flexibility with IDs (#54)
    * add flexibility
    
    * v0.4.12

--- src/button/types.ts
+++ src/button/types.ts
@@ -1,6 +1,10 @@
 import { ReactNode } from 'react';
 
 export interface BaseButtonProps {
+  /**
+   * A custom id for ally, analytics
+   */
+  id?: string;
   /** Whether the button is disabled. */
   isDisabled?: boolean;
   /** The content to display in the button. */
