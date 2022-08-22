a7222485aeac7f95b8c699cf86bf44667c5464fa
mikeldking <mikeldking@gmail.com>
2022-08-22 17:47:38 -0600

    simplify

--- src/button/types.ts
+++ src/button/types.ts
@@ -1,7 +1,13 @@
-import { ButtonHTMLAttributes, ReactNode } from 'react';
+import { ReactNode } from 'react';
+import { AriaLabelingProps, FocusableDOMProps } from '../types';
 
-export interface BaseButtonProps
-  extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface AriaBaseButtonProps extends FocusableDOMProps, AriaLabelingProps {
+  /**
+   * The behavior of the button when used in an HTML form.
+   */
+  type?: 'button' | 'submit' | 'reset';
+}
+export interface BaseButtonProps extends AriaBaseButtonProps {
   /** Whether the button is disabled. */
   isDisabled?: boolean;
   /** The content to display in the button. */
