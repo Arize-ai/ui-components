a73ab614cc4e2ef434ce2c531a0bfe6b45f4e440
Kunal Shah <kunal@arize.com>
2022-12-07 16:47:57 -0800

    WIP

--- stories/DropdownWithForm.stories.tsx
+++ stories/DropdownWithForm.stories.tsx
@@ -33,8 +33,7 @@ const meta: Meta = {
     controls: { expanded: true },
     design: {
       type: 'figma',
-      url:
-        'https://www.figma.com/file/Zz1WXrITnlcYnIs55prawf/H2---Monitors?node-id=867%3A7920&t=5fgjUv2krdFhxCMZ-1',
+      url: 'https://www.figma.com/file/Zz1WXrITnlcYnIs55prawf/H2---Monitors?node-id=867%3A7920&t=5fgjUv2krdFhxCMZ-1',
     },
   },
 };
@@ -44,59 +43,76 @@ export default meta;
 function DropdownMenu() {
   let options = [
     { id: 1, name: 'Quantiles' },
-    { id: 2, name: 'Num Bins' },
-    { id: 3, name: 'Discrete Bins' },
+    { id: 2, name: 'Discrete Bins' },
+    { id: 3, name: 'Number of Bins' },
+    { id: 4, name: 'Custom Bins' },
   ];
   let [binningStrategy, setBinningStrategy] = useState<number>(1);
 
   const strategyNeedsForm = useMemo(() => {
-    return binningStrategy === 2;
+    return binningStrategy === 3 || binningStrategy === 4;
   }, [binningStrategy]);
 
   useEffect(() => {
     window.dispatchEvent(new Event('resize'));
   }, [strategyNeedsForm]);
 
+  const textFieldLabel =
+    binningStrategy === 3 ? 'Number of Bins' : 'Custom Bins';
+
   return (
     <div
       css={css`
         display: flex;
-        flex-direction: row;
-        width: ${!strategyNeedsForm ? 400 : 200}px;
+        flex-direction: row-reverse;
         justify-content: flex-end;
       `}
     >
-      <section
-        css={css`
-          padding: 16px;
-          box-sizing: border-box;
-          transition: width 0.3s ease-in-out;
-          &[data-hidden='true'] {
-            width: 0;
-            overflow: hidden;
-            padding: 0;
-        `}
-        data-hidden={!strategyNeedsForm}
-      >
-        <Form>
-          <TextField label="Bins" />
-        </Form>
-        <div>
-          <Button variant="default">Apply</Button>
-        </div>
-      </section>
       <ListBox
-        style={{ width: 200, flex: 'none' }}
+        style={{ width: 250, flex: 'none' }}
         aria-label="Binning strategy"
         items={options}
         selectionMode="single"
-        onSelectionChange={selection => {
+        onSelectionChange={(selection) => {
           const id = [...selection][0];
           setBinningStrategy(parseInt(id as string, 10));
         }}
       >
-        {item => <Item key={item.id}>{item.name}</Item>}
+        {(item) => <Item key={item.id}>{item.name}</Item>}
       </ListBox>
+      <section
+        css={css`
+          padding: 12px;
+          box-sizing: border-box;
+          transition: width 0.3s ease-in-out;
+          border-right: 1px solid #768ca3;
+          &[data-hidden='true'] {
+            width: 0;
+            overflow: hidden;
+            padding: 0;
+            border-right: none;
+        `}
+        data-hidden={!strategyNeedsForm}
+      >
+        <Form>
+          <TextField label={textFieldLabel} />
+        </Form>
+        <div
+          css={css`
+            border-top: 1px solid #768ca3;
+          `}
+        >
+          <Button
+            css={css`
+              margin-left: auto;
+              margin-top: 12px;
+            `}
+            variant="primary"
+          >
+            Apply
+          </Button>
+        </div>
+      </section>
     </div>
   );
 }
@@ -110,9 +126,9 @@ export const DropdownWithForm = () => (
       <Dropdown
         menu={<DropdownMenu />}
         triggerProps={{ placement: 'bottom right', isOpen: true }}
-        buttonProps={{ addonBefore: 'Binning', style: { maxWidth: 250 } }}
+        buttonProps={{ addonBefore: 'Bin Option', style: { maxWidth: 250 } }}
       >
-        Your selection here
+        Select Binning
       </Dropdown>
     </div>
   </Provider>
