012611fd133315f5f97358f084bc28219a7e081b
Mikyo King <mikyo@arize.com>
2023-03-23 21:42:07 -0600

    feat: icons for book, download

--- src/icon/Icons.tsx
+++ src/icon/Icons.tsx
@@ -579,3 +579,73 @@ export const ListOutline = () => (
     </g>
   </svg>
 );
+
+export const BookOpenFilled = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g data-name="Layer 2">
+      <g data-name="book-open">
+        <rect
+          width="24"
+          height="24"
+          transform="rotate(180 12 12)"
+          opacity="0"
+        />
+        <path d="M21 4.34a1.24 1.24 0 0 0-1.08-.23L13 5.89v14.27l7.56-1.94A1.25 1.25 0 0 0 21.5 17V5.32a1.25 1.25 0 0 0-.5-.98z" />
+        <path d="M11 5.89L4.06 4.11A1.27 1.27 0 0 0 3 4.34a1.25 1.25 0 0 0-.48 1V17a1.25 1.25 0 0 0 .94 1.21L11 20.16z" />
+      </g>
+    </g>
+  </svg>
+);
+
+export const BookFilled = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g data-name="Layer 2">
+      <g data-name="book">
+        <rect width="24" height="24" opacity="0" />
+        <path d="M19 3H7a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM7 19a1 1 0 0 1 0-2h11v2z" />
+      </g>
+    </g>
+  </svg>
+);
+
+export const BookOutline = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g data-name="Layer 2">
+      <g data-name="book">
+        <rect width="24" height="24" opacity="0" />
+        <path d="M19 3H7a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM7 5h11v10H7a3 3 0 0 0-1 .18V6a1 1 0 0 1 1-1zm0 14a1 1 0 0 1 0-2h11v2z" />
+      </g>
+    </g>
+  </svg>
+);
+
+export const Download = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g data-name="Layer 2">
+      <g data-name="download">
+        <rect width="24" height="24" opacity="0" />
+        <rect x="4" y="18" width="16" height="2" rx="1" ry="1" />
+        <rect
+          x="3"
+          y="17"
+          width="4"
+          height="2"
+          rx="1"
+          ry="1"
+          transform="rotate(-90 5 18)"
+        />
+        <rect
+          x="17"
+          y="17"
+          width="4"
+          height="2"
+          rx="1"
+          ry="1"
+          transform="rotate(-90 19 18)"
+        />
+        <path d="M12 15a1 1 0 0 1-.58-.18l-4-2.82a1 1 0 0 1-.24-1.39 1 1 0 0 1 1.4-.24L12 12.76l3.4-2.56a1 1 0 0 1 1.2 1.6l-4 3a1 1 0 0 1-.6.2z" />
+        <path d="M12 13a1 1 0 0 1-1-1V4a1 1 0 0 1 2 0v8a1 1 0 0 1-1 1z" />
+      </g>
+    </g>
+  </svg>
+);
