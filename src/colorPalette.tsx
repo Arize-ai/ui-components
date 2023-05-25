b5f30aa44af7e06ed955722bac49eb09c7d02fc4
Mikyo King <mikyo@arize.com>
2023-05-25 12:57:36 -0600

    comment out colors

--- src/colorPalette.tsx
+++ src/colorPalette.tsx
@@ -1,17 +1,33 @@
-export const purpleColors = {
-  purple900: '	rgb(192, 143, 252)',
-  purple800: 'rgb(178, 119, 250)',
-  purple700: '#AE8DF8',
-  purple600: '#A17AF7',
-  purple500: '#9468F6',
-  purple400: '#8755F4',
-  purple300: '#7A43F3',
-  purple200: '#6D30F2',
-  purple100: '#5F1EF1',
-} as const;
+// TODO to be filled out by andy
+// export const purpleColors = {
+//   purple1200: '#EFE9FE',
+//   purple1100: '#E2D6FC',
+//   purple1000: '#D5C4FB',
+//   purple900: '#C8B1FA',
+//   purple800: '#BB9FF9',
+//   purple700: '#AE8DF8',
+//   purple600: '#A17AF7',
+//   purple500: '#9468F6',
+//   purple400: '#8755F4',
+//   purple300: '#7A43F3',
+//   purple200: '#6D30F2',
+//   purple100: '#5F1EF1',
+// } as const;
 
+// TODO to be filled out by Andy
 // export const turquoiseColors = {
-//   turquoise900: '#9efcfd',
+//   turquoise1200: '#EFE9FE',
+//   turquoise1100: '#E2D6FC',
+//   turquoise1000: '#D5C4FB',
+//   turquoise900: '#C8B1FA',
+//   turquoise800: '#BB9FF9',
+//   turquoise700: '#AE8DF8',
+//   turquoise600: '#A17AF7',
+//   turquoise500: '#',
+//   turquoise400: '#51FAFB',
+//   turquoise300: '#3EF9FB',
+//   turquoise200: '#2BF8FB',
+//   turquoise100: '#17F8FA',
 // } as const;
 
 export const grayColors = {
