f48206c61b86fcbea4263a26f1ebacc1fb161c1a
Mikyo King <mikyo@arize.com>
2023-05-25 12:57:51 -0600

    comment out colors

--- src/colorPalette.tsx
+++ src/colorPalette.tsx
@@ -1,35 +1,3 @@
-// TODO to be filled out by andy
-// export const purpleColors = {
-//   purple1200: '#EFE9FE',
-//   purple1100: '#E2D6FC',
-//   purple1000: '#D5C4FB',
-//   purple900: '#C8B1FA',
-//   purple800: '#BB9FF9',
-//   purple700: '#AE8DF8',
-//   purple600: '#A17AF7',
-//   purple500: '#9468F6',
-//   purple400: '#8755F4',
-//   purple300: '#7A43F3',
-//   purple200: '#6D30F2',
-//   purple100: '#5F1EF1',
-// } as const;
-
-// TODO to be filled out by Andy
-// export const turquoiseColors = {
-//   turquoise1200: '#EFE9FE',
-//   turquoise1100: '#E2D6FC',
-//   turquoise1000: '#D5C4FB',
-//   turquoise900: '#C8B1FA',
-//   turquoise800: '#BB9FF9',
-//   turquoise700: '#AE8DF8',
-//   turquoise600: '#A17AF7',
-//   turquoise500: '#',
-//   turquoise400: '#51FAFB',
-//   turquoise300: '#3EF9FB',
-//   turquoise200: '#2BF8FB',
-//   turquoise100: '#17F8FA',
-// } as const;
-
 export const grayColors = {
   gray900: '#181B1F',
   gray800: '#1d2126',
