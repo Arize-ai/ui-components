238ee62b5e4f82d06eea44c7bb0b70ba84ff828d
Mikyo King <mikyo@arize.com>
2023-06-09 17:31:35 -0600

    fix :infotip padding (#133)
    * fix: contextual help size
    
    * v0.12.2

--- src/icon/Icons.tsx
+++ src/icon/Icons.tsx
@@ -702,3 +702,30 @@ export const SunOutline = () => (
     </g>
   </svg>
 );
+
+export const ArrowDownFilled = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g data-name="Layer 2">
+      <g data-name="arrow-downward">
+        <rect
+          width="24"
+          height="24"
+          transform="rotate(-90 12 12)"
+          opacity="0"
+        />
+        <path d="M12 17a1.72 1.72 0 0 1-1.33-.64l-4.21-5.1a2.1 2.1 0 0 1-.26-2.21A1.76 1.76 0 0 1 7.79 8h8.42a1.76 1.76 0 0 1 1.59 1.05 2.1 2.1 0 0 1-.26 2.21l-4.21 5.1A1.72 1.72 0 0 1 12 17z" />
+      </g>
+    </g>
+  </svg>
+);
+
+export const ArrowUpFilled = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g data-name="Layer 2">
+      <g data-name="arrow-up">
+        <rect width="24" height="24" transform="rotate(90 12 12)" opacity="0" />
+        <path d="M16.21 16H7.79a1.76 1.76 0 0 1-1.59-1 2.1 2.1 0 0 1 .26-2.21l4.21-5.1a1.76 1.76 0 0 1 2.66 0l4.21 5.1A2.1 2.1 0 0 1 17.8 15a1.76 1.76 0 0 1-1.59 1z" />
+      </g>
+    </g>
+  </svg>
+);
