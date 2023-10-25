08dfd53849dee770a1e8be506ea9e8bb713178c5
Mikyo King <mikyo@arize.com>
2023-10-25 13:45:29 -0600

    alert colors

--- src/alert/styles.ts
+++ src/alert/styles.ts
@@ -1,20 +1,16 @@
 import { css } from '@emotion/react';
-import { transparentize, darken } from 'polished';
-import theme from '../theme';
+import { SeverityLevel } from '../types';
 
 export const baseSeverityCSS = css`
   backdrop-filter: blur(10px);
 `;
 
-const generateSeverityCSS = (
-  semanticColor: 'info' | 'warning' | 'success' | 'danger'
-) => {
+const generateSeverityCSS = (semanticColor: SeverityLevel) => {
   const colorPrefix = `--ac-global-color-${semanticColor}`;
   return css(
     baseSeverityCSS,
     css`
       border: 1px solid var(${colorPrefix});
-      /* FireFox Only style */
       background-color: var(${colorPrefix}-700);
       color: var(--ac-global-color-text-900);
     `
