87031f88316b2dfdf575c50d686b06ea69a25725
Mikyo King <mikyo@arize.com>
2024-10-14 09:00:36 -0600

    cleanup

--- stories/Accordion.stories.tsx
+++ stories/Accordion.stories.tsx
@@ -9,6 +9,7 @@ import {
   Counter,
   Text,
   Button,
+  View,
 } from '../src';
 import InfoTip from './components/InfoTip';
 import { ThemeSplitView } from './components/ThemeSplitView';
@@ -22,14 +23,17 @@ const meta: Meta = {
 };
 
 const AccordionContents = () => (
-  <Text>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-  </Text>
+  <View padding="size-100">
+    <Text>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum
+    </Text>
+  </View>
 );
 
 export default meta;
