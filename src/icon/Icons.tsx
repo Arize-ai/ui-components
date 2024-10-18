af5e35ab7d0efac43c425baf7d7657dd9004835c
Parker Stafford <52351508+Parker-Stafford@users.noreply.github.com>
2024-10-18 14:48:01 -0700

    feat: add message and message filled icons (#246)

--- src/icon/Icons.tsx
+++ src/icon/Icons.tsx
@@ -2169,7 +2169,27 @@ export const MinusOutline = () => (
   </svg>
 );
 
-//W
-//X
-//Y
-//Z
+export const MessageSquareOutline = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g data-name="Layer 2">
+      <g data-name="message-square">
+        <rect width="24" height="24" opacity="0" />
+        <circle cx="12" cy="11" r="1" />
+        <circle cx="16" cy="11" r="1" />
+        <circle cx="8" cy="11" r="1" />
+        <path d="M19 3H5a3 3 0 0 0-3 3v15a1 1 0 0 0 .51.87A1 1 0 0 0 3 22a1 1 0 0 0 .51-.14L8 19.14a1 1 0 0 1 .55-.14H19a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3zm1 13a1 1 0 0 1-1 1H8.55a3 3 0 0 0-1.55.43l-3 1.8V6a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1z" />
+      </g>
+    </g>
+  </svg>
+);
+
+export const MessageSquareFilled = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g data-name="Layer 2">
+      <g data-name="message-square">
+        <rect width="24" height="24" opacity="0" />
+        <path d="M19 3H5a3 3 0 0 0-3 3v15a1 1 0 0 0 .51.87A1 1 0 0 0 3 22a1 1 0 0 0 .51-.14L8 19.14a1 1 0 0 1 .55-.14H19a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3zM8 12a1 1 0 1 1 1-1 1 1 0 0 1-1 1zm4 0a1 1 0 1 1 1-1 1 1 0 0 1-1 1zm4 0a1 1 0 1 1 1-1 1 1 0 0 1-1 1z" />
+      </g>
+    </g>
+  </svg>
+);
